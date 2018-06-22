3f1a365defe197fa058269d86d72a38f66978ece
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-22 07:17:27 -0400

    Asset Management
    Setup
    npm install webpack webpack-cli --save-dev
    
    Loading CSS
    npm install --save-dev style-loader css-loader
    npm install sass-loader node-sass webpack --save-dev
    
    Loading Images
    npm install --save-dev file-loader
    
    Loading Data
    npm install --save-dev csv-loader xml-loader

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# portfolio
+# Asset Management
+
+Setup
+npm install webpack webpack-cli --save-dev
+
+Loading CSS
+npm install --save-dev style-loader css-loader
+npm install sass-loader node-sass webpack --save-dev
+
+Loading Images
+npm install --save-dev file-loader
+
+Loading Data
+npm install --save-dev csv-loader xml-loader
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  }
+};
